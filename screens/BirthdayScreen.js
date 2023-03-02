b992940f619240e9a901221e1c426c54660420c1
ethanj12 <ethanjones0624@gmail.com>
2023-03-01 20:31:55 -0500

    Styled the Birthday Screen

--- screens/BirthdayScreen.js
+++ screens/BirthdayScreen.js
@@ -17,22 +17,24 @@ export default function BirthdayScreen({ navigation, route}) {
   return (
     <ImageBackground source={{uri: 'https://i.postimg.cc/cJ45GdKH/background1.png'}} style={styles.imageBackground}>
       <View style={styles.container}>
-        <View style={styles.innerPlacard}>
-          <TouchableHighlight style={styles.deleteButton} underlayColor='#f00' onPress={() => {deletePerson(id)}}>
-            <Text style={styles.buttonText}>Delete</Text>
-          </TouchableHighlight>
-          <View style={styles.allButBottomButton}>
-          <View style={styles.placeholderImage}/>
-          <Text style={{fontSize: 40, paddingTop:10}}>{name}</Text>
-          <Text style={{fontSize: 40, paddingTop:5}}>{birthday_month}</Text>
-          <Text style={{fontSize: 40, paddingTop:5}}>{birthday_day}</Text>
-          <Text style={{fontSize: 20, paddingTop:5}}>{notes}</Text>
-        </View>
-        <TouchableHighlight style={styles.createButton} onPress={() => navigation.navigate("HomeScreen")}>
+        <TouchableHighlight style={styles.backButton} onPress={() => navigation.navigate("HomeScreen")}>
           <Text style={styles.buttonText}>Go Back</Text>
         </TouchableHighlight>
+          <View style={styles.allButBottomButton}>
+            <View style={styles.placeholderImage}/>
+            <Text style={styles.nameStyle}>{name}</Text>
+            <View style={styles.monthday}>
+              <Text style={styles.monthStyle}>Birthday: {birthday_month} / {birthday_day}</Text>
+            </View>
+            <View style={styles.notesContainer}>
+              <Text style={{flex: 1, fontSize: 20, marginRight: 10}}>Notes: </Text>
+              <Text style={{flex: 4, fontSize: 20}}>{notes.length == 0 ? 'No Notes' : notes}</Text>
+            </View>    
         </View>
-      </View>
+        <TouchableHighlight style={styles.deleteButton} underlayColor='#f00' onPress={() => {deletePerson(id)}}>
+            <Text style={styles.buttonText}>Delete</Text>
+          </TouchableHighlight>
+        </View>
     </ImageBackground>
   );
 }
@@ -45,20 +47,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  deleteButton : {
+  notesContainer : {
+    height: 200, 
+    width: '100%',  
+    textAlign: 'justify',
+    width: 300,
+    flexDirection: 'row'
+  },
+  monthStyle : {
+    textAlign: 'center', 
+    width: '100%',
+    fontSize: 40,
+  },
+  monthday : {
+    flexDirection: 'row',
+    marginBottom: 20,
+    height: 50,
+    justifyContent: 'space-between',
+  },
+  nameStyle: {
+    height: 50, 
+    fontSize: 40,  
+    textAlign: 'center', 
+    width: 300,
+    marginBottom: 20
+  },
+  backButton : {
     height: 50,
     width: '100%',
-    backgroundColor: '#f00',
+    backgroundColor: '#000',
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 10
   },
-  createButton : {
+  deleteButton : {
     height: 50,
     width: '100%',
-    backgroundColor: '#000',
+    backgroundColor: '#f00',
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
     alignItems: 'center',
@@ -81,12 +108,11 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    marginTop: '30%',
-    marginBottom: '20%',
-    paddingTop: 10,
+    marginTop: '15%',
+    marginBottom: '40%',
     width: '95%',
     height: '80%',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center'
@@ -94,7 +120,8 @@ const styles = StyleSheet.create({
   placeholderImage : {
     height: 250,
     width: 250,
-    backgroundColor: '#000'
+    backgroundColor: '#000',
+    marginBottom: 20
   }
 
 });
